314223102e29d6313f040f7e647b696240ae91aa
François Billet <francoisbillet@live.fr>
2023-02-03 09:51:06 +0100

    Exercise 6 extra credit 3

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -7,24 +7,24 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
-  const [status, setStatus] = React.useState('idle')
+  const [{status, pokemon, error}, setState] = React.useState({
+    status: 'idle',
+    pokemon: null,
+    error: null,
+  })
 
   React.useEffect(() => {
     if (!pokemonName) {
       return
     }
-    setStatus('pending')
+    setState({status: 'pending'})
 
     fetchPokemon(pokemonName)
       .then(pokemonData => {
-        setPokemon(pokemonData)
-        setStatus('resolved')
+        setState({status: 'resolved', pokemon: pokemonData})
       })
       .catch(e => {
-        setError(e)
-        setStatus('rejected')
+        setState({status: 'rejected', error: e})
       })
   }, [pokemonName])
 
